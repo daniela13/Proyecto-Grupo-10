07aede9cb7d7ff3cf09adbc0740e6803a6d812f7
Lab-214-21 <Lab-214-21@Lab-214-21-PC>
2013-06-17 20:33:25 -0400

    inicio del proyecto

---
+++ src/pentagrama/Pentagrama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pentagrama;
+
+/**
+ *
+ * @author Lab-214-21
+ */
+public class Pentagrama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo java...");
+    }
+    
+}
